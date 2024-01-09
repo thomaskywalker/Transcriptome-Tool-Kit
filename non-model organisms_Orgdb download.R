4915b78e78fe437a1355d2a49d6ea96936211228
Poaceae1999 <b07601009@g.ntu.edu.tw>
2024-01-09 16:54:44 +0800

    add a simple script for search non-model organisms.

---
+++ non-model organisms_Orgdb download.R
@@ -0,0 +1,6 @@
+# library(AnnotationHub)
+# library(AnnotationDbi)
+# ah <- AnnotationHub()
+# query(ah, "Hordeum vulgare") 
+# ah[['AH114278']] # $rdataclass: OrgDb
+# saveDb(ah[['AH114278']],'Orgdb')
